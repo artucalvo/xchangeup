872c7360ab8152be6da43977f84e7f13dc595262
Arturo Calvo <arturocalvodevesa@gmail.com>
2015-10-21 15:13:57 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -14,19 +14,20 @@ $lang = '';
 if(isset($_GET['lang'])) { 
 	$lang = "?lang=".$_GET['lang'];
 }
+echo 'Test:'.$lang;
 
 if ($page_name=='') {
+    echo $browser_t.'/index.html'.$lang;
 	include $browser_t.'/index.html'.$lang;
-	}
+}
 elseif ($page_name=='privacy.html') {
 	include 'privacy.html'.$lang;
-	}
+}
 elseif ($page_name=='terms.html') {
 	include 'terms.html'.$lang;
-	}
-else
-	{
-		include $browser_t.'/index.html'.$lang;
-	}
+}
+else {
+	include $browser_t.'/index.html'.$lang;
+}
 
 ?>

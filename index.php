7e52d0bd49588d61cb36a547e032e44b1dbc933d
Arturo Calvo <arturocalvodevesa@gmail.com>
2015-10-21 15:23:11 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -10,24 +10,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 include "app/config.php";
 include "app/detect.php";
 
-$lang = '';
-if(isset($_GET['lang'])) { 
-	$lang = "?lang=".$_GET['lang'];
-}
-echo 'Test:'.$lang;
-
-if ($page_name=='') {
-    echo $browser_t.'/index.html'.$lang;
-	include $browser_t.'/index.html'.$lang;
-}
-elseif ($page_name=='privacy.html') {
-	include 'privacy.html'.$lang;
-}
-elseif ($page_name=='terms.html') {
-	include 'terms.html'.$lang;
-}
-else {
-	include $browser_t.'/index.html'.$lang;
-}
+include $browser_t.$page_name;
 
 ?>

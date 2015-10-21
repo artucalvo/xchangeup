6b1251358795aed0637645ec43d7a0c976316b56
Arturo Calvo <arturocalvodevesa@gmail.com>
2015-10-21 16:16:54 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -10,22 +10,32 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 include "app/config.php";
 include "app/detect.php";
 
-$lang = '';
-if(isset($_GET['lang'])) { 
-	$lang = "?lang=".$_GET['lang'];
+session_start();
+
+$languages = array('es', 'en');
+
+// handle language selection
+
+if(isset($_GET['lang'])) { // Set session language is specified
+  if(in_array($_GET['lang'], $languages)) {
+    $_SESSION['lang'] = $_GET['lang'];
+  }
+  else { // Default language
+    $_SESSION['lang'] = $languages[0];
+  }
 }
 
 if ($page_name=='') {
-	include $browser_t.'/index.html'.$lang;
+	include $browser_t.'/index.html';
 }
 elseif ($page_name=='privacy.html') {
-	include 'privacy.html'.$lang;
+	include 'privacy.html';
 }
 elseif ($page_name=='terms.html') {
-	include 'terms.html'.$lang;
+	include 'terms.html';
 }
 else {
-	include $browser_t.'/index.html'.$lang;
+	include $browser_t.'/index.html';
 }
 
 ?>

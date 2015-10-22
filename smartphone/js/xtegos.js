1875586f95e8d5e8007f5cf9f49fb9137ff4d260
Arturo Calvo Devesa <arturo.calvodevesa@sedicii.com>
2015-10-22 03:19:38 +0100

    modified:   smartphone/index.html

--- smartphone/js/xtegos.js
+++ smartphone/js/xtegos.js
@@ -9,7 +9,14 @@
 	$(document).ready(function() {
 		$().UItoTop({ easingType: 'easeOutQuart' });
 	});
+	
+	addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }
 
+	//	The menu on the left
+	$(function() {
+		$('nav#menu-left').mmenu();
+	});
+	
 	
 /* Google Analytics */
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
